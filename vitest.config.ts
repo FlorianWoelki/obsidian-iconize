0215093f7ed68c20c9b9e2ee38720343ebe12b6c
FlorianWoelki <FlorianWoelki@gmx.de>
2023-10-15 17:02:58 +0200

    build: add unit tests and pnpm install workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
     setupFiles: ['./src/test-setup.ts'],
     environment: 'happy-dom',
     coverage: {
+      reporter: ['text', 'json-summary', 'json'],
       provider: 'istanbul',
+      lines: 70,
+      branches: 70,
+      functions: 70,
+      statements: 70,
     },
   },
   resolve: {
