4d87cade3e713daa0fc47efc8d408375eb984136
s-crypt <41712656+s-crypt@users.noreply.github.com>
2024-01-29 08:34:05 -0800

    Update icon pack download URLs to newest version (#360)
    * Update icon pack download URLs to newest version
    
    * Update old paths
    
    oops, missed one

--- src/icon-packs.ts
+++ src/icon-packs.ts
@@ -9,30 +9,30 @@ const iconPacks = {
   faBrands: {
     name: 'font-awesome-brands',
     displayName: 'FontAwesome Brands',
-    path: 'fontawesome-free-6.4.2-web/svgs/brands/',
+    path: 'fontawesome-free-6.5.1-web/svgs/brands/',
     downloadLink:
-      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.4.2/fontawesome-free-6.4.2-web.zip',
+      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.5.1/fontawesome-free-6.5.1-web.zip',
   },
   faRegular: {
     name: 'font-awesome-regular',
     displayName: 'FontAwesome Regular',
-    path: 'fontawesome-free-6.4.2-web/svgs/regular/',
+    path: 'fontawesome-free-6.5.1-web/svgs/regular/',
     downloadLink:
-      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.4.2/fontawesome-free-6.4.2-web.zip',
+      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.5.1/fontawesome-free-6.5.1-web.zip',
   },
   faSolid: {
     name: 'font-awesome-solid',
     displayName: 'FontAwesome Solid',
-    path: 'fontawesome-free-6.4.2-web/svgs/solid/',
+    path: 'fontawesome-free-6.5.1-web/svgs/solid/',
     downloadLink:
-      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.4.2/fontawesome-free-6.4.2-web.zip',
+      'https://github.com/FortAwesome/Font-Awesome/releases/download/6.5.1/fontawesome-free-6.5.1-web.zip',
   },
   remixIcons: {
     name: 'remix-icons',
     displayName: 'Remix Icons',
     path: '',
     downloadLink:
-      'https://github.com/Remix-Design/RemixIcon/releases/download/v3.5.0/RemixIcon_Svg_v3.5.0.zip',
+      'https://github.com/Remix-Design/RemixIcon/releases/download/v4.1.0/RemixIcon_Svg_v4.1.0.zip',
   },
   iconBrew: {
     name: 'icon-brew',
@@ -45,23 +45,23 @@ const iconPacks = {
   simpleIcons: {
     name: 'simple-icons',
     displayName: 'Simple Icons',
-    path: 'simple-icons-9.14.0/icons',
+    path: 'simple-icons-11.2.0/icons',
     downloadLink:
-      'https://github.com/simple-icons/simple-icons/archive/refs/tags/9.14.0.zip',
+      'https://github.com/simple-icons/simple-icons/archive/refs/tags/11.2.0.zip',
   },
   lucide: {
     name: 'lucide-icons',
     displayName: 'Lucide',
     path: '',
     downloadLink:
-      'https://github.com/lucide-icons/lucide/releases/download/0.277.0/lucide-icons-0.277.0.zip',
+      'https://github.com/lucide-icons/lucide/releases/download/0.314.0/lucide-icons-0.314.0.zip',
   },
   tablerIcons: {
     name: 'tabler-icons',
     displayName: 'Tabler Icons',
     path: 'svg',
     downloadLink:
-      'https://github.com/tabler/tabler-icons/releases/download/v2.34.0/tabler-icons-2.34.0.zip',
+      'https://github.com/tabler/tabler-icons/releases/download/v2.46.0/tabler-icons-2.46.0.zip',
   },
   /** @source https://boxicons.com/ */
   boxicons: {
