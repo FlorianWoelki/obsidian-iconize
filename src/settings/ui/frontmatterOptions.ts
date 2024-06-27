10f26a2128df35b5cd71f7c658c3437cab5011dc
Adam Langbert <adamhl@pm.me>
2024-06-27 02:43:53 -0500

    feat(ui): add setting to refresh icons from frontmatter (#507)
    * feat(ui): add setting to refresh icons from frontmatter
    
    * fix: don't include plugin name in logger message

--- src/settings/ui/frontmatterOptions.ts
+++ src/settings/ui/frontmatterOptions.ts
@@ -1,6 +1,9 @@
 import { Setting, TextComponent } from 'obsidian';
 import IconFolderSetting from './iconFolderSetting';
 import { Notice } from 'obsidian';
+import config from '@app/config';
+import { isHexadecimal, stringToHex } from '@app/util';
+import { logger } from '@app/lib/logger';
 
 export default class FrontmatterOptions extends IconFolderSetting {
   private iconFieldNameTextComp: TextComponent;
@@ -71,6 +74,75 @@ export default class FrontmatterOptions extends IconFolderSetting {
           this.plugin.getSettings().iconColorInFrontmatterFieldName = newValue;
           await this.plugin.saveIconFolderData();
           new Notice('...saved successfully');
+        });
+      });
+
+    new Setting(this.containerEl)
+      .setName('Refresh icons from frontmatter')
+      .setDesc(
+        'Sets the icon and color for each note in the vault based on the frontmatter properties. WARNING: This will change any manually set icons to the one defined in the frontmatter. IF A NOTE HAS NO FRONTMATTER, THE CURRENT ICON WILL BE REMOVED. Please restart Obsidian after this completes to see the changes.',
+      )
+      .addButton((btn) => {
+        btn.setButtonText('Refresh').onClick(async () => {
+          if (!this.plugin.getSettings().iconInFrontmatterEnabled) {
+            new Notice(
+              `[${config.PLUGIN_NAME}] Please enable "Use icon in frontmatter".`,
+            );
+            return;
+          }
+
+          new Notice(
+            `[${config.PLUGIN_NAME}] Refreshing icons from frontmatter, please wait...`,
+          );
+
+          const files = this.plugin.app.vault.getMarkdownFiles();
+
+          for (const file of files) {
+            const fileCache = this.plugin.app.metadataCache.getFileCache(file);
+
+            const frontmatterIconKey =
+              this.plugin.getSettings().iconInFrontmatterFieldName;
+            const frontmatterIconColorKey =
+              this.plugin.getSettings().iconColorInFrontmatterFieldName;
+
+            const iconName = fileCache.frontmatter?.[frontmatterIconKey];
+            let iconColor = fileCache.frontmatter?.[frontmatterIconColorKey];
+
+            if (!iconName) {
+              await this.plugin.removeFolderIcon(file.path);
+              continue;
+            }
+
+            if (typeof iconName !== 'string') {
+              const message = `${file.path}\nFrontmatter property type \`${frontmatterIconKey}\` has to be of type \`text\`.`;
+              logger.warn(message);
+              new Notice(`[${config.PLUGIN_NAME}]\n${message}`);
+              continue;
+            }
+
+            this.plugin.addFolderIcon(file.path, iconName);
+
+            if (!iconColor) {
+              await this.plugin.removeIconColor(file.path);
+              continue;
+            }
+
+            if (typeof iconColor !== 'string') {
+              const message = `${file.path}\nFrontmatter property type \`${frontmatterIconColorKey}\` has to be of type \`text\`.`;
+              logger.warn(message);
+              new Notice(`[${config.PLUGIN_NAME}]\n${message}`);
+              continue;
+            }
+
+            iconColor = isHexadecimal(iconColor)
+              ? stringToHex(iconColor)
+              : iconColor;
+
+            this.plugin.addIconColor(file.path, iconColor);
+          }
+          new Notice(
+            `[${config.PLUGIN_NAME}] Refreshed icons from frontmatter. Please restart Obsidian to see the changes.`,
+          );
         });
       });
   }
