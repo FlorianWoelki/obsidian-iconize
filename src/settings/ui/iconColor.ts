195162f1db642604f33d2c2fbcaaefe8cbd54704
FlorianWoelki <FlorianWoelki@gmx.de>
2024-07-06 09:57:25 +0200

    feat(ui): add custom ResetButtonComponent

--- src/settings/ui/iconColor.ts
+++ src/settings/ui/iconColor.ts
@@ -1,34 +1,32 @@
 import { Setting, ColorComponent } from 'obsidian';
 import IconFolderSetting from './iconFolderSetting';
 import helper from '../helper';
+import { ResetButtonComponent } from '../ResetButtonComponent';
+import { DEFAULT_SETTINGS } from '../data';
+
+const DEFAULT_VALUE = DEFAULT_SETTINGS.iconColor;
 
 export default class IconColorSetting extends IconFolderSetting {
   public display(): void {
-    const colorCustomization = new Setting(this.containerEl)
+    const setting = new Setting(this.containerEl)
       .setName('Icon color')
       .setDesc('Change the color of the displayed icons.');
-    const colorPicker = new ColorComponent(colorCustomization.controlEl)
-      .setValue(this.plugin.getSettings().iconColor ?? '#000000')
+
+    new ResetButtonComponent(setting.controlEl).onClick(async () => {
+      colorPicker.setValue(DEFAULT_VALUE);
+      this.plugin.getSettings().iconColor = null;
+      // Custom saving to not save the color black in the data.
+      await this.plugin.saveIconFolderData();
+      helper.refreshStyleOfIcons(this.plugin);
+    });
+
+    const colorPicker = new ColorComponent(setting.controlEl)
+      .setValue(this.plugin.getSettings().iconColor ?? DEFAULT_VALUE)
       .onChange(async (value) => {
         this.plugin.getSettings().iconColor = value;
         await this.plugin.saveIconFolderData();
 
         helper.refreshStyleOfIcons(this.plugin);
       });
-
-    colorCustomization.addButton((button) => {
-      button
-        .setButtonText('Default')
-        .setTooltip('Set color to the default one')
-        .onClick(async () => {
-          colorPicker.setValue('#000000');
-          this.plugin.getSettings().iconColor = null;
-          await this.plugin.saveIconFolderData();
-
-          helper.refreshStyleOfIcons(this.plugin);
-        });
-    });
-
-    colorCustomization.components.push(colorPicker);
   }
 }
